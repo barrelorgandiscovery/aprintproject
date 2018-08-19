c157588ca51ef471feb0cf96eacfcab06d064ff5
Patrice Freydiere <frett27@gmail.com>
2018-08-19 17:40:23 +0200

    add test for serial

--- aprint-machine-control-gui/src/main/java/org/barrelorgandiscovery/perfo/gui/PerfoCommander.java
+++ aprint-machine-control-gui/src/main/java/org/barrelorgandiscovery/perfo/gui/PerfoCommander.java
@@ -1,17 +1,19 @@
 package org.barrelorgandiscovery.perfo.gui;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 
 public class PerfoCommander {
 
-  public PerfoCommander() {
-    // TODO Auto-generated constructor stub
-  }
+  public PerfoCommander() {}
 
-  public static void main(String[] args) {
-	  JFrame f = new JFrame();
-	  f.setSize(320, 200);
-	  f.setVisible(true);
-	  f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  public static void main(String[] args) throws Exception {
+    JFrame f = new JFrame();
+    f.setSize(320, 200);
+    f.setVisible(true);
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.getContentPane().setLayout(new BorderLayout());
+    f.getContentPane().add(new JMainPanel(), BorderLayout.CENTER);
   }
 }
