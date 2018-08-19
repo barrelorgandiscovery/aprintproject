ea0fec5189e62e7220e22f840408401e8c390994
Patrice Freydiere <frett27@gmail.com>
2018-08-19 17:30:53 +0200

    add perfo commander

---
+++ aprint-machine-control-gui/src/main/java/org/barrelorgandiscovery/perfo/gui/PerfoCommander.java
@@ -0,0 +1,17 @@
+package org.barrelorgandiscovery.perfo.gui;
+
+import javax.swing.JFrame;
+
+public class PerfoCommander {
+
+  public PerfoCommander() {
+    // TODO Auto-generated constructor stub
+  }
+
+  public static void main(String[] args) {
+	  JFrame f = new JFrame();
+	  f.setSize(320, 200);
+	  f.setVisible(true);
+	  f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+}
