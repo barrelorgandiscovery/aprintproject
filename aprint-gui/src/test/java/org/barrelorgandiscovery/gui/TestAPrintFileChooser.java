f5997526dd09b42f5e7adfee7406376aad2bf28e
Patrice Freydiere <frett27@gmail.com>
2020-08-29 10:26:05 +0200

    add test on filechooser

---
+++ aprint-gui/src/test/java/org/barrelorgandiscovery/gui/TestAPrintFileChooser.java
@@ -0,0 +1,13 @@
+package org.barrelorgandiscovery.gui;
+
+import org.barrelorgandiscovery.gui.tools.APrintFileChooser;
+
+public class TestAPrintFileChooser {
+
+	public static void main(String[] args) throws Exception 
+	{
+		APrintFileChooser fc = new APrintFileChooser();
+		fc.showOpenDialog(null);
+	}
+	
+}
