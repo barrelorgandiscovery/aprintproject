422ebe252468de4eef466db3d6d0ec017d7ef20c
Patrice Freydiere <frett27@gmail.com>
2023-05-07 10:35:48 +0200

    add instrument filtering with name in create

--- aprint-gui/src/main/java/org/barrelorgandiscovery/gui/aprintng/APrintNG.java
+++ aprint-gui/src/main/java/org/barrelorgandiscovery/gui/aprintng/APrintNG.java
@@ -17,6 +17,7 @@ import java.awt.dnd.DropTargetEvent;
 import java.awt.dnd.DropTargetListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.print.PageFormat;
@@ -1598,6 +1599,15 @@ public class APrintNG extends APrintNGInternalFrame implements ActionListener, A
 						}, Messages.getString("APrintNG.4000"), this.repository.getRepository2()); // $NON-NLS-1$
 
 				intf.getContentPane().add(p, BorderLayout.CENTER);
+				
+//				intf.addComponentListener(new ComponentAdapter() {
+//					@Override
+//					public void componentResized(ComponentEvent e) {
+//						super.componentResized(e);
+//						p.invalidate();
+//					}
+//				});
+				
 				intf.setVisible(true);
 
 			} else if ("SEARCH".equals(e.getActionCommand())) { //$NON-NLS-1$
